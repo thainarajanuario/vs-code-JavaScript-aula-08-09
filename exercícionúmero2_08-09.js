3abe759d24141cbfbde565c4617677acf5cf0bbb
thainarajanuario <gigitata199427@gmail.com>
2022-09-08 13:31:33 -0300

    exercicios-08-09

---
+++ exercícionúmero2_08-09.js
@@ -0,0 +1,13 @@
+let nome , peso , altura , DI , diabético
+
+nome = prompt ("Escreva seu nome?");
+peso = prompt ("Qual é o seu peso?");
+altura = prompt ("Qual é a sua altura?");
+DI = prompt ("Qual é a data da sua internação?");
+diabético = prompt ("você é diabético?");
+
+alert ("Seu nome é " + nome);
+alert ("Seu peso é " + peso);
+alert ("Sua altura é " + altura);
+alert ("A data da sua internação é " + DI);
+alert ("É diabético " + diabético)
