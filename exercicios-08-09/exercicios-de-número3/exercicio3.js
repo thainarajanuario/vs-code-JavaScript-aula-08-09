a3e9f4cad80c8889372b919fd25987ad87e85946
thainarajanuario <gigitata199427@gmail.com>
2022-09-08 16:49:39 -0300

    Exercicios final

---
+++ exercicios-08-09/exercicios-de-número3/exercicio3.js
@@ -0,0 +1,39 @@
+/*let Preço_de_custo , Valor_da_venda , Porcentual
+
+Preço_de_custo = prompt ("Qual é o preço de custo? ");
+Porcentual = prompt ("Qual é o Porcentual? ")
+
+Valor_da_venda = Preço_de_custo * Porcentual/100 + Preço_de_custo
+alert ("O valor da venda  é " + Valor_da_venda )*/
+
+
+
+/*let Valor_depositado , Valor_de_rend
+
+Valor_depositado = prompt ("Qual é o valor depositado? ");
+alert("O valor depositado após o juros é " + Valor_depositado * 1.007)*/
+
+
+
+/*let Real , Dolar , Cotação
+
+Dolar = prompt ("Digite o valor em Dolar que você quer converter: ");
+Cotação = prompt ("O valor da Cotação é? ");
+
+Real = Dolar * Cotação
+alert ("O valor do real é " + Real)*/
+
+
+let Número
+
+Número = prompt ("Qual é o número? ")
+
+if (Número > 80){
+    alert("Este número é maior que 80")
+
+}else if(Número < 25){
+alert("Este número é menor que 25")}
+
+else if (Número = 40)
+alert ("Este número é igual a 40")
+
---
+++ js08-09aula de explicação.js
@@ -0,0 +1,22 @@
+var variavel = 45 // inteiro
+var variavel = 49.5 // real
+var variavel = "Programadores Cariocas"
+var variavel = "A"
+var variavel = false
+
+const pi = 3.1416;
+
+
+
+// Operadores aritimeticos
+// (+ , - , * , / , %)
+
+// Operadores relacionais
+// (== , >= , <= , > , < , !=)
+
+// Operadores logico
+// (e ou (&& ||)
+
+// Incremento e descremento
+//(++ --)
+
